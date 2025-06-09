7f5fc12e8e37c65110674a6a50780b1d0a925a6e
RmNLcN06 <romain.luciano@orange.fr>
2025-06-09 11:35:59 +0200

    Add Actor & Serie ShortDTOs

---
+++ src/main/java/com/example/project_critics_backend/DTOs/ActorShortDTO.java
@@ -0,0 +1,7 @@
+package com.example.project_critics_backend.DTOs;
+
+public class ActorShortDTO {
+    public Long id;
+    public String lastname;
+    public String firstname;
+}
---
+++ src/main/java/com/example/project_critics_backend/DTOs/SerieShortDTO.java
@@ -0,0 +1,7 @@
+package com.example.project_critics_backend.DTOs;
+
+public class SerieShortDTO {
+    public Long id;
+    public String title;
+    public int year;
+}
