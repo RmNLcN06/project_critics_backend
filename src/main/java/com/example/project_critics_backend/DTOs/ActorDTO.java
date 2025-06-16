3ab36bc28cdc797bfd279cbb1c4a884af39a0a89
RmNLcN06 <romain.luciano@orange.fr>
2025-06-16 21:39:44 +0200

    Add Validation in Actor & Serie DTOs

--- src/main/java/com/example/project_critics_backend/DTOs/ActorDTO.java
+++ src/main/java/com/example/project_critics_backend/DTOs/ActorDTO.java
@@ -1,12 +1,23 @@
 package com.example.project_critics_backend.DTOs;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+
 import java.time.LocalDate;
 import java.util.Set;
 
 public class ActorDTO {
     public Long id;
+
+    @NotBlank(message = "The actor's lastname is required")
     public String lastname;
+
+    @NotBlank(message = "The actor's firstname is required")
     public String firstname;
+
+    @Past(message = "The date of birth must be in the past")
     public LocalDate dateOfBirth;
-    public Set<SerieShortDTO> series;
+
+    public Set<@Valid SerieShortDTO> series;
 }
--- src/main/java/com/example/project_critics_backend/DTOs/SerieDTO.java
+++ src/main/java/com/example/project_critics_backend/DTOs/SerieDTO.java
@@ -1,12 +1,20 @@
 package com.example.project_critics_backend.DTOs;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.List;
 import java.util.Set;
 
 public class SerieDTO {
     public Long id;
+
+    @NotBlank(message = "The serie's title is required")
     public String title;
+
     public int year;
-    public List<String> types;
-    public Set<ActorShortDTO> actors;
+
+    public List<@NotBlank(message = "A type cannot be empty") String> types;
+
+    public Set<@Valid ActorShortDTO> actors;
 }
